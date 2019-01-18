308ed82c336b90abec63ffd5967a4b025f8748ee
Petrovicks <41557867+Petrovicks@users.noreply.github.com>
2019-01-18 11:08:28 -0500

    Attempting to add better error handling if usb throws a random error. Threw a giant chunk of main() in a try: block, if it fails, an exception will attempt to remount the usb and continue.

--- main.py
+++ main.py
@@ -170,97 +170,103 @@ def main():
     count = 0
     count2 = 0
     while check_usb(): #Continue running as long as USB is inserted.
-        if validTime:
-            #Time Stuff - Create new folder after time limit
-            if eventTime-startTime >= 60*10: #If time is greater than 600 seconds
-                if _USE_HW_CLOCK == False:
-                    print "Getting RTC time.."
-                    try:
-                        rtc_time = clock.read_datetime() #Get time from RTC
-                    except:
-                        check_clock_validity(clock, True)
-                    bashCommand = "date -s '" + rtc_time.strftime("%Y-%m-%d %H:%M:%S") + "'"
-                    subprocess.call(bashCommand, shell=True) #Sync Pi HW clock with RTC
-                else:
-                    print "Using HW clock..."
-                    rtc_time = datetime.datetime.now()
-                rtc_time = rtc_time.timetuple()
-                dirName = "%04d-%02d-%02d--%02d-%02d-%02d"%(rtc_time[0],rtc_time[1],rtc_time[2],rtc_time[3],rtc_time[4],rtc_time[5])
-                fullDN = '/media/usb/'+dirName
-                os.mkdir(fullDN)
-                startTime = (rtc_time[3]*60*60) +(rtc_time[4]*60) +(rtc_time[5])
+        #Very dirty, hopefully temporary as a measure to combat unexpected USB miscommunication.
+        try:
+            if validTime:
+                #Time Stuff - Create new folder after time limit
+                if eventTime-startTime >= 60*10: #If time is greater than 600 seconds
+                    if _USE_HW_CLOCK == False:
+                        print "Getting RTC time.."
+                        try:
+                            rtc_time = clock.read_datetime() #Get time from RTC
+                        except:
+                            check_clock_validity(clock, True)
+                        bashCommand = "date -s '" + rtc_time.strftime("%Y-%m-%d %H:%M:%S") + "'"
+                        subprocess.call(bashCommand, shell=True) #Sync Pi HW clock with RTC
+                    else:
+                        print "Using HW clock..."
+                        rtc_time = datetime.datetime.now()
+                    rtc_time = rtc_time.timetuple()
+                    dirName = "%04d-%02d-%02d--%02d-%02d-%02d"%(rtc_time[0],rtc_time[1],rtc_time[2],rtc_time[3],rtc_time[4],rtc_time[5])
+                    fullDN = '/media/usb/'+dirName
+                    os.mkdir(fullDN)
+                    startTime = (rtc_time[3]*60*60) +(rtc_time[4]*60) +(rtc_time[5])
 
-            #Enforce one second increments between pictures using Pi HW clock
-            dTime = 0
-            while dTime < 1:
-                currentTime = datetime.datetime.now()
-                currentTime = currentTime.timetuple()
-                t1 = (currentTime[3]*60*60) + (currentTime[4]*60) + (currentTime[5])
-                dTime = abs(t1 - eventTime)
-            eventTime = t1
-            timez = "%02d-%02d-%02d"%(currentTime[3],currentTime[4],currentTime[5])
-            if clockOk == True:
-                GPIO.output(speaker, True)
-                time.sleep(0.1)
-                GPIO.output(speaker,False)
+                #Enforce one second increments between pictures using Pi HW clock
+                dTime = 0
+                while dTime < 1:
+                    currentTime = datetime.datetime.now()
+                    currentTime = currentTime.timetuple()
+                    t1 = (currentTime[3]*60*60) + (currentTime[4]*60) + (currentTime[5])
+                    dTime = abs(t1 - eventTime)
+                eventTime = t1
+                timez = "%02d-%02d-%02d"%(currentTime[3],currentTime[4],currentTime[5])
+                if clockOk == True:
+                    GPIO.output(speaker, True)
+                    time.sleep(0.1)
+                    GPIO.output(speaker,False)
+                else:
+                    for i in range(3):
+                        GPIO.output(speaker, True)
+                        time.sleep(0.05)
+                        GPIO.output(speaker, False)
+                        time.sleep(0.05)
             else:
-                for i in range(3):
+                #Error with reading RTC or HW clock
+                if count2 == 0:
+                    for name in os.listdir('/media/usb/'):
+                        if name[:8] == 'noclock_':
+                            count3 = int(name[8:])
+                            if count3 > count2:
+                                count2 = count3
+                if eventTime-startTime >= 60*10:
+                    count2 += 1
+                    dirName = 'noclock_'+str(count2)
+                    fullDN = '/media/usb/'+dirName
+                    os.mkdir(fullDN)
+                    startTime = time.time()
+                timez = "0"
+                for i in range(2):
                     GPIO.output(speaker, True)
                     time.sleep(0.05)
-                    GPIO.output(speaker, False)
+                    GPIO.output(speaker,False)
                     time.sleep(0.05)
-        else:
-            #Error with reading RTC or HW clock
-            if count2 == 0:
-                for name in os.listdir('/media/usb/'):
-                    if name[:8] == 'noclock_':
-                        count3 = int(name[8:])
-                        if count3 > count2:
-                            count2 = count3
-            if eventTime-startTime >= 60*10:
-                count2 += 1
-                dirName = 'noclock_'+str(count2)
-                fullDN = '/media/usb/'+dirName
-                os.mkdir(fullDN)
-                startTime = time.time()
-            timez = "0"
-            for i in range(2):
-                GPIO.output(speaker, True)
-                time.sleep(0.05)
-                GPIO.output(speaker,False)
-                time.sleep(0.05)
-        if mode == 1:
-            image,fpa,aux = capture(flip_v=False, device = "/dev/spidev0.1")
-            cv2.imwrite(fullDN+'/image_thermal_%s_%09d.png'%(timez,count),image)
-            #Capture and save from the rgb camera
-            camera.capture(fullDN+'/image_rgb_%s_%09d.jpg'%(timez,count))#,quality=10)
-            generate_header(fullDN,'image_thermal_%s_%09d'%(timez,count),timez,fpa,aux)
-            count += 1
-        elif mode == 2:
-            camera.start_recording(fullDN+'/video_rgb_%s_%09d.h264'%(timez,count))
-            camera.wait_recording(10)
-            camera.stop_recording()
-            print 'Video captured'
-        elif mode == 3:
-            image,fpa,aux = capture(flip_v=False, device = "/dev/spidev0.1")
-            cv2.imwrite(fullDN+'/image_thermal_%s_%09d.png'%(timez,count),image)
-            print 'Thermal captured'
-            generate_header(fullDN,'image_thermal_%s_%09d'%(timez,count),timez,fpa,aux)
-
-    #If USB is no longer mounted it is out of storage.
-    #TODO: Make more robust, the usb check assumes it is mounted as sda or sda1
-    mounted = False
-    raise TypeError('USB out of memory or disconnected.')
-    focalPlane = numpy.ones((1))*1.0
-    fpa = focalPlane*fpa
-    auxTemp = numpy.ones((1))*1.0
-    aux = auxTemp*aux
-    p = (fullDN+'/fpa_%s_%09d.txt'%(timez,count))
-    a = (fullDN+'/aux_%s_%09d.txt'%(timez,count))
-    numpy.savetxt(p,fpa)
-    numpy.savetxt(a,aux)
-    print'Elapsed time='+ str(eventTime - startTime) + '[s]'
-    print ' '
+            if mode == 1:
+                image,fpa,aux = capture(flip_v=False, device = "/dev/spidev0.1")
+                cv2.imwrite(fullDN+'/image_thermal_%s_%09d.png'%(timez,count),image)
+                #Capture and save from the rgb camera
+                camera.capture(fullDN+'/image_rgb_%s_%09d.jpg'%(timez,count))#,quality=10)
+                generate_header(fullDN,'image_thermal_%s_%09d'%(timez,count),timez,fpa,aux)
+                count += 1
+            elif mode == 2:
+                camera.start_recording(fullDN+'/video_rgb_%s_%09d.h264'%(timez,count))
+                camera.wait_recording(10)
+                camera.stop_recording()
+                print 'Video captured'
+            elif mode == 3:
+                image,fpa,aux = capture(flip_v=False, device = "/dev/spidev0.1")
+                cv2.imwrite(fullDN+'/image_thermal_%s_%09d.png'%(timez,count),image)
+                print 'Thermal captured'
+                generate_header(fullDN,'image_thermal_%s_%09d'%(timez,count),timez,fpa,aux)
+        except Exception, e:
+            #If USB is no longer mounted it is out of storage.
+            #TODO: Make more robust, the usb check assumes it is mounted as sda or sda1
+            if mount_usb():
+                pass
+            else:
+                raise TypeError(str(e))
+    #I'm not sure what all this is for, I'm thinking for other modes. Doesn't seem to be
+    #needed for this specific application.
+    # focalPlane = numpy.ones((1))*1.0
+    # fpa = focalPlane*fpa
+    # auxTemp = numpy.ones((1))*1.0
+    # aux = auxTemp*aux
+    # p = (fullDN+'/fpa_%s_%09d.txt'%(timez,count))
+    # a = (fullDN+'/aux_%s_%09d.txt'%(timez,count))
+    # numpy.savetxt(p,fpa)
+    # numpy.savetxt(a,aux)
+    # print'Elapsed time='+ str(eventTime - startTime) + '[s]'
+    # print ' '
 
 if __name__ == '__main__':
     #Pass these variables as globals
